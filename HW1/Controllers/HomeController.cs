f065637098fcce5730745d1cb94c5ecec3d49a14
Ho Wing Wong <wongh5@seattleu.edu>
2019-05-17 18:10:27 -0700

    Added Model to Home Controller

--- HW1/Controllers/HomeController.cs
+++ HW1/Controllers/HomeController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using HW1.Models;
 namespace HW1.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            var myData = new HomeViewModel
+            {
+                PhoneCount = 10,
+                UserCount = 15,
+                LastAcessDate = DateTime.Now
+            };
             return View();
         }
 
