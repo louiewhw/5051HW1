b2a34bbb2e890cb36d9ed6470438ce0db0299a76
Ho Wing Wong <wongh5@seattleu.edu>
2019-05-17 18:13:19 -0700

    Hooked up the Model to Home and Index View

--- HW1/Controllers/HomeController.cs
+++ HW1/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace HW1.Controllers
                 UserCount = 15,
                 LastAcessDate = DateTime.Now
             };
-            return View();
+            return View(myData);
         }
 
         public ActionResult FAQ()
